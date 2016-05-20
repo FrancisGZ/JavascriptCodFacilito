8ab5b4a2e70655b83ba7629afc0b2d3f59e4b9c4
Francis <francisgz50@gmail.com>
2016-05-19 22:06:14 -0500

    Se agregomovimiento a las barras

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 	self.Board.prototype = {
 		get elements(){
 			var elements = this.bars;
-			elements.push(this.ball);
+			//elements.push(this.ball);
 			return elements;
 		}
 	}
@@ -60,39 +60,55 @@
 	}
 
 	self.BoardView.prototype = {
+
+		clean:function ()
+		{
+			this.ctx.clearRect(0,0,this.board.width, this.board.height);
+		},
+
+
+
 		draw: function(){
 			for (var i = this.board.elements.length - 1; i >= 0; i--) {
 				var el = this.board.elements[i];
 
 				draw(this.ctx,el);
 			};
+		},
+
+		play: function()
+		{
+			board_view.clean();
+			board_view.draw();
 		}
 	}
 
 	function draw (ctx,element){
-		if (element !== null && element.hasOwnProperty("kind")){
+		//if (element !== null && element.hasOwnProperty("kind")){
 
 			switch(element.kind){
 			case "rectangle":
 				ctx.fillRect(element.x,element.y,element.width,element.height);
 			break;
 			}
-		} 
+		//} 
 		
 	}
 })();
 
 
-
 	var board = new Board(800,400);
 	var bar = new Bar(20,100, 40,100,board);
-	var bar = new Bar(735,100, 40,100,board)
+	var bar_2 = new Bar(735,100, 40,100,board)
 	var canvas = document.getElementById('canvas');
 	var board_view = new BoardView(canvas, board);
+	
+
+	
 
  document.addEventListener("keydown",function(ev){
  	
-
+ 	ev.preventDefault();
  	if(ev.keyCode==38)
  	{
  		bar.up();
@@ -101,13 +117,34 @@
  	{
  		bar.down();
  	}
+ 	else if(ev.keyCode===87)
+ 	{
+ 		bar_2.up();//w
+ 	}
+ 	else if (ev.keyCode===83)
+ 	{
+ 		bar_2.down();//s
+ 	}
+
+
  	console.log(bar.toString());
  });
-window.addEventListener("load", main);
 
 
-function main (){
+
+
+//self.addEventListener("load", main);
+
+
+window.requestAnimationFrame(controller);
+
+
+
+function controller (){
+	
+	board_view.play();
+	
 	
 
-	board_view.draw();
+	window.requestAnimationFrame(controller);
 }
